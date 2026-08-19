236f8c80187cc281815e35acac21d2cc582a0125
Hess Wang <heisuewang@gmail.com>
2026-08-19 23:12:43 +0900

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.ts
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s: string): number {
+        const chars = new Set<string>();
+        let left = 0;
+        let longest = 0;
+
+        for (let right = 0; right< s.length; right++){
+            while(chars.has(s[right])){
+                chars.delete(s[left]);
+                left++;
+            }
+
+            chars.add(s[right]);
+
+            longest = Math.max(
+                longest,
+                right - left +1
+            );
+        }
+
+        return longest;
+    }
+}
