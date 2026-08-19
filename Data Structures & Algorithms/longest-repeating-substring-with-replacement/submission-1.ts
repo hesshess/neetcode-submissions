902b16cc9f64c1fd866a4aecd1b204d49e38e1d5
Hess Wang <heisuewang@gmail.com>
2026-08-19 23:21:19 +0900

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.ts
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s: string, k: number): number {
+                const count = new Map<string, number>();
+
+        let left = 0;
+        let maxFreq = 0;
+        let result = 0;
+
+        for (let right = 0; right < s.length; right++) {
+            const char = s[right];
+
+            count.set(char, (count.get(char) || 0) + 1);
+            maxFreq = Math.max(maxFreq, count.get(char)!);
+
+            while ((right - left + 1) - maxFreq > k) {
+                count.set(s[left], count.get(s[left])! - 1);
+                left++;
+            }
+
+            result = Math.max(result, right - left + 1);
+        }
+
+        return result;
+       
+       
+    }
+}
