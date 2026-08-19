d909f91de7aa9468016888ed24adf361affb281e
Hess Wang <heisuewang@gmail.com>
2026-08-19 23:12:59 +0900

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.ts
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices: number[]): number {
+        let minPrice = Infinity;
+        let maxProfit = 0;
+
+        for (const price of prices){
+            minPrice = Math.min(minPrice, price);
+            maxProfit = Math.max(maxProfit, price - minPrice);
+        }
+            return maxProfit;
+    }
+}
